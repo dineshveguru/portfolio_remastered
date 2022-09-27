ac6944d43b89659a5a33e8dc9ec66df85c7b249f
Dinesh <dineshveguru123@gmail.com>
2022-09-27 22:53:06 +0530

    started hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,21 @@
 function Hero() {
   return (
-    <div className="hero-container">
-      <h1>Hello, There</h1>
-      <h1>
-        I am <span>Dinesh</span>
-      </h1>
-      <p>Designer + Developer</p>
-      <button>Download Resume</button>
-      <p>Scroll down</p>
-      <img src={require(`../asset/scroll_dark.png`)} />
+    <div className="hero-container hero--dark">
+      <div className="name-banner">
+        <h1>Hello, there👋</h1>
+        <h1>
+          I am <span>Dinesh</span>
+        </h1>
+        <p className="skill-banner">
+          <span className="designer">Designer</span> +
+          <span className="developer">Developer</span>
+        </p>
+        <button>Download Resume</button>
+      </div>
+      <div className="scroll-banner">
+        <p>Scroll down</p>
+        <img src={require(`../asset/scroll_dark.png`)} />
+      </div>
     </div>
   );
 }
