678e2b40a9f2abf62c82e4224ada820350a7bf0a
Dinesh <dineshveguru123@gmail.com>
2022-09-28 15:56:59 +0530

    added extra

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,27 @@
 function Hero() {
   return (
-    <div className="hero-container hero--dark">
-      <div className="name-banner">
-        <h1>Hello, there👋</h1>
-        <h1>
-          I am <span>Dinesh</span>
-        </h1>
-        <p className="skill-banner">
-          <span className="designer">Designer</span>+
-          <span className="developer">Developer</span>
-        </p>
-        <button className="resume-download--button">Download Resume</button>
+    <div className="hero-top">
+      <div className="hero-container hero--dark">
+        <div className="name-banner">
+          <h1>Hello, there👋</h1>
+          <h1>
+            I am <span>Dinesh</span>
+          </h1>
+          <p className="skill-banner">
+            <span className="designer">Designer</span>+
+            <span className="developer">Developer</span>
+          </p>
+          <button className="resume-download--button">Download Resume</button>
+        </div>
+        <div className="scroll-banner">
+          <p>Scroll down</p>
+          <img
+            src={require(`../asset/scroll_light.png`)}
+            className="scroll-down--image"
+          />
+        </div>
+        <div className="blob"></div>
       </div>
-      <div className="scroll-banner">
-        <p>Scroll down</p>
-        <img
-          src={require(`../asset/scroll_light.png`)}
-          className="scroll-down--image"
-        />
-      </div>
-      <div className="blob"></div>
     </div>
   );
 }
